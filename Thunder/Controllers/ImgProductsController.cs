f3fccbf74cdd03acc194d177684a7295d44ed106
Dang Tuan Huy <dangtuanhuyhachi@gmail.com>
2018-07-04 12:45:52 +0700

    Update Source

---
+++ Thunder/Controllers/ImgProductsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Thunder.Models;
+
+namespace Thunder.Controllers
+{
+    public class ImgProductsController : Controller
+    {
+        private ShoppingCartEntities db = new ShoppingCartEntities();
+
+        // GET: ImgProducts
+        public ActionResult Index()
+        {
+            var imgProducts = db.ImgProducts.Include(i => i.Product);
+            return View(imgProducts.ToList());
+        }
+
+        // GET: ImgProducts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ImgProduct imgProduct = db.ImgProducts.Find(id);
+            if (imgProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(imgProduct);
+        }
+
+        // GET: ImgProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProduceCode");
+            return View();
+        }
+
+        // POST: ImgProducts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ImgId,ImgProduct1,ProductId")] ImgProduct imgProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ImgProducts.Add(imgProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProduceCode", imgProduct.ProductId);
+            return View(imgProduct);
+        }
+
+        // GET: ImgProducts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ImgProduct imgProduct = db.ImgProducts.Find(id);
+            if (imgProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProduceCode", imgProduct.ProductId);
+            return View(imgProduct);
+        }
+
+        // POST: ImgProducts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ImgId,ImgProduct1,ProductId")] ImgProduct imgProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(imgProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProduceCode", imgProduct.ProductId);
+            return View(imgProduct);
+        }
+
+        // GET: ImgProducts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ImgProduct imgProduct = db.ImgProducts.Find(id);
+            if (imgProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(imgProduct);
+        }
+
+        // POST: ImgProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ImgProduct imgProduct = db.ImgProducts.Find(id);
+            db.ImgProducts.Remove(imgProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Thunder/Controllers/SupplierController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Thunder.Models;
+
+namespace Thunder.Controllers
+{
+    public class SupplierController : Controller
+    {
+        //Khai báo đối tượng Entity
+        private ShoppingCartEntities DB;
+       //Hàm khởi tạo Entity
+        public SupplierController()
+        {
+            DB = new ShoppingCartEntities ();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Khi dùng ajax để gọi thì Trang Index sẽ hiển thị đầu tiên (Ajax gồm get, set các đối tượng)
+        public ActionResult GetSupplier()
+        {
+            //Lấy ra danh sách các nhà cung cấp
+            var Supplier = DB.Suppliers.ToList();
+            //Trả về đối tượng Json với phương thức get
+            return Json(Supplier, JsonRequestBehavior.AllowGet);
+
+        }
+        //Trả về đúng các tham số đã truyền vào 
+        public ActionResult GetSupplierId(int Id)
+        {
+            var Supplier = DB.Suppliers.ToList().Find(m => m.SupplierId == Id);
+            return Json(Supplier, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude = "SupplierId")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                DB.Suppliers.Add(supplier);
+                DB.SaveChanges();
+            }
+
+            return Json(supplier, JsonRequestBehavior.AllowGet);
+        }
+        // Chỉnh sửa thông tin của một record
+        [HttpPost]
+        public ActionResult Update(Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                DB.Entry(supplier).State = EntityState.Modified;
+                DB.SaveChanges();
+            }
+
+            return Json(supplier, JsonRequestBehavior.AllowGet);
+        }
+
+
+        // Xóa record tương ứng với tham số id đã truyền vào
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var supplier = DB.Suppliers.ToList().Find(m => m.SupplierId == id);
+
+            if (supplier != null)
+            {
+                DB.Suppliers.Remove(supplier);
+                DB.SaveChanges();
+            }
+
+            return Json(supplier, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
